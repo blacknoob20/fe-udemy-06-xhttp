a53575ce27d2e9cadae1e223239e312e00f19c1b
Cristhian Guerrero <cristian.guerrero@guayas.gob.ec>
2021-05-16 14:44:16 -0500

    Optimizacion del uso del fetch()

--- src/index.js
+++ src/index.js
@@ -1,13 +1,21 @@
 const chuckNorrisJokes = 'https://api.chucknorris.io/jokes/random';
 
 // fetch hace una peticion a una URL, devuelve una promesa.
-fetch(chuckNorrisJokes).then((resp)=>{
+// fetch(chuckNorrisJokes).then((resp)=>{
     // La funcion json tambien devuleve una promesa
     // resp.json().then(datos => {
     //     console.log(datos.id, datos.value);
     // });
     // Otra manera de hacer
-    resp.json().then(({id, value}) => {
-        console.log(id, value);
-    });
-});
+    // resp.json().then(({id, value}) => {
+    //     console.log(id, value);
+    // });
+// });
+
+// Otra manera mas optimizada
+fetch(chuckNorrisJokes)
+    .then(resp => resp.json())
+    .then(({id, value}) => {
+        console.log(id);
+        console.log(value);
+    })
