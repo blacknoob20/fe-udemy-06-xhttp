75b28d6c82a79d0686dfb49ecbe1151589807764
Cristhian Guerrero <cristian.guerrero@guayas.gob.ec>
2021-05-16 20:51:11 -0500

    Obtener los usuarios del API/Rest

--- src/index.js
+++ src/index.js
@@ -1,3 +1,4 @@
-import { init } from "./js/usuarios-page";
+// Esta sentencia indica que todo lo que se exporte en crud-providers se va a guardar en el objeto CRUD
+import * as CRUD from "./js/crud-providers";
 
-init();
+CRUD.getUsuario(1).then(console.log);
