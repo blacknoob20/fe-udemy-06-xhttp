ba0d952f339c23e0a4ed9c5ddc6047a11585cb41
Cristhian Guerrero <cristian.guerrero@guayas.gob.ec>
2021-05-17 10:49:23 -0500

    Creo el HTML para subir los archivos

--- src/index.js
+++ src/index.js
@@ -1,18 +1,3 @@
-// Esta sentencia indica que todo lo que se exporte en crud-providers se va a guardar en el objeto CRUD
-import * as CRUD from "./js/crud-providers";
+import { init } from "./js/archivos-page";
 
-const crisId = 604;
-
-// CRUD.getUsuario(1).then(console.log);
-
-// CRUD.insUsuario({
-//     "name": "Cristhian",
-//     "job": "Developer"
-// }).then(console.log);
-
-// CRUD.actUsuario(crisId,{
-//     "name": "Cristhian",
-//     "job": "PHP Developer"
-// }).then(console.log);
-
-CRUD.eliUsuario(crisId).then(console.log);
+init();
---
+++ src/js/archivos-page.js
@@ -0,0 +1,32 @@
+const body = document.body;
+let inputFile, fileFoto;
+
+const crearHtlm = () => {
+    const html = `
+    <h1 class="mt-5">Subir Archivos</h1>
+    <hr>
+    <label>Seleccionar una foto:</label>
+    <input type="file" accept="image/png, image/jpeg" />
+    <br>
+    <img id="foto" src="" class="img-thumbnail" />
+    `;
+
+    const div = document.createElement('div');
+    
+    div.innerHTML = html;
+    body.append(div);
+    inputFile = document.querySelector('input');
+    fileFoto  = document.querySelector('#foto');
+}
+
+const eventos = () => {
+    inputFile.addEventListener('change', (evt) => {
+        const file = evt.target.files[0];
+        console.log(file);
+    })
+}
+
+export const init = () => {
+    crearHtlm();
+    eventos();
+}
